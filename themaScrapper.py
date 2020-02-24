12dcf6c6feb9d2c0a85db250307956b249ce61e0
Leesh <korea201003@naver.com>
2020-02-24 23:24:54 +0900

    테마 관련 수정사항 반영

--- themaScrapper.py
+++ themaScrapper.py
@@ -42,9 +42,10 @@ def themeScrap():
         print("Load Thema info : ", url)
         # 총 5 페이지의 테마 섹터에 대하여 조사 (Default 1 Page)
         theme_code = 0
-        for page in range(2, 5): 
+        for page in range(2, 8): 
             # 앞의 30개는 유효하지 않으므로 뒤의 30개에 대해서 선택
-            for index in range(30, 60):
+            
+            for index in range(0, 30):
                 xpath = "//td[@class='ellipsis']//a[starts-with(@href, 'javascript')]"
                 elements = driver.find_elements_by_xpath(xpath)
                 theme_name = elements[index].text
@@ -77,17 +78,15 @@ def themeScrap():
                 conn.commit()
                 theme_code += 1
                 driver.back()
-
+            
             # 다음 페이지 버튼 클릭
-            xpath = "//a[@href='#' and text()='{0}']".format(page)
-            driver.find_element_by_xpath(xpath).click()
+            if page == 5 :
+                xpath = "//a[@href='#' and @class='after']".format(page)
+                driver.find_element_by_xpath(xpath).click()
+            else :
+                xpath = "//a[@href='#' and text()='{0}']".format(page)
+                driver.find_element_by_xpath(xpath).click()
             
-            
-        # 주식 재무제표 데이터 일자 가져오기
-        sql = "SELECT TOP 1 A.[DATE] FROM FINANCE_INFO A ORDER BY [DATE] DESC"
-        cur.execute(sql)
-        date_t =  cur.fetchall()
-        date = datetime.strptime(date_t[0][0], '%Y-%m-%d').date()
     finally:
         conn.close()
 
